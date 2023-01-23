eb1c32325c882b1d0b3c97ad7faf2747bd98fba6
fweise <public@weise-games.de>
2023-01-23 19:31:09 +0100

    update commander

--- src/utils/commander.ts
+++ src/utils/commander.ts
@@ -4,16 +4,16 @@ import { Event } from "./event";
 import { Stopwatch } from "./stopwatch";
 import { formatDuration } from "../other";
 
-export class Commander {
-    public readonly onMessage = new Event<Commander, string>();
+export class Commander<TArgs, TRes> {
+    public readonly onMessage = new Event<Commander<TArgs, TRes>, string>();
 
-    private readonly _commands: NodeJS.Dict<Singleton<Command<any, any>>> = {};
+    private readonly _commands: NodeJS.Dict<Singleton<Command<TArgs, TRes>>> = {};
 
-    public add(command: string, singleton: Singleton<Command<any, any>>) {
+    public add(command: string, singleton: Singleton<Command<TArgs, TRes>>) {
         this._commands[command.toLowerCase()] = singleton;
     }
 
-    public async execute<TArgs, TRes>(command: string, args: TArgs): Promise<TRes> {
+    public async execute<TArgs2 extends TArgs, TRes2 extends TRes>(command: string, args: TArgs2): Promise<TRes2> {
         const commandLine = `${command} ${Object.keys(args).map(key => `--${key} ${args[key]}`).join(' ')}`;
         const stopwatch = new Stopwatch();
 
@@ -33,11 +33,15 @@ export class Commander {
 
             this.onMessage.emit(this, `commander >> ${commandLine} >> ${result} (${formatDuration(stopwatch.duration, { seconds: true, milliseconds: true })})`);
 
-            return result as TRes;
+            return result as TRes2;
         } catch (error) {
             this.onMessage.emit(this, `commander >> ${commandLine} >> ${error.stack}`);
 
             throw error;
         }
+    }
+
+    public hasCommand<TArgs, TRes>(name: string): boolean {
+        return !!this._commands[name.toLowerCase()];
     }
 }
